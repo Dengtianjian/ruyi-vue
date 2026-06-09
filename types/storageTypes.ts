59b87121de0f84fa3da73e75a4aa11f00cd32ee3
DengTianjian <mail@isdtj.com>
2026-06-09 14:56:21 +0800

    更新存储类

--- types/storageTypes.ts
+++ types/storageTypes.ts
@@ -3,26 +3,33 @@ export type TStorageQuery = Record<string, string | number>;
 
 export type TStorageAccessControl = 'private' | 'public-read' | 'public-read-write' | 'authenticated-read' | 'authenticated-read-write';
 
-export interface TStorageFileInfo {
+export interface TStorageFile {
   id?: number,
   key: string,
-  remote: boolean,
-  belongsId: string,
-  belongsType: string,
-  ownerId: number,
-  sourceFileName: string,
+  remote?: boolean,
+  belongs_id?: string,
+  belongs_type?: string,
+  owner_id?: number,
+  source_file_name: string,
   name: string,
   size: number,
-  path: string,
+  path?: string,
   width: number,
   height: number,
   extension: string,
-  accessControl: TStorageAccessControl,
-  createdAt?: number,
-  updatedAt?: number,
+  access_control: TStorageAccessControl,
+  link: string,
+  url: string,
+  sort_order: number,
+  created_at?: number,
+  updated_at?: number
+}
 
-  previewURL?: string,
-  downloadURL?: string,
-  remotePreviewURL?: string,
-  downloadDownloadURL?: string
+export type StorageSignParams = {
+  "sign-algorithm": string,
+  "sign-time": string,
+  "key-time": string,
+  "header-list": string,
+  signature: string,
+  "url-param-list": string
 }
